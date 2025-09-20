e1f7b07ddf530ce4a5c0a481a0144bb9eae34080
aditya-investnama <aditya@investnama.com>
2025-09-20 08:03:53 +0530

    modified

--- backend/eventhandler.js
+++ backend/eventhandler.js
@@ -6,45 +6,11 @@ const onRequestEvent = (req, res) => {
 
   if (req.url === '/' && req.method === 'PUT') {
 
-        return res.end('You havent added any endpoint try adding one!');
+    return res.end('You havent added any endpoint try adding one!');
 
   } else if(req.url === '/create-user' && req.method === 'PUT'){  
     
-    const body = [];
-
-    req.on('data', (chunck)=>{
-        body.push(chunck);
-    });
-
-    req.on('end',async ()=>  {
-      const fullBodyAsString = Buffer.concat(body).toString(); 
-
-
-      const jsonObject = JSON.parse(fullBodyAsString);
-
-      const count = fs.readdirSync('./userfiles',(err,data)=>{
-        return data;
-      });
-
-
-      for(let file of count){
-        if(file==`${jsonObject.phone}.txt`){
-           res.statusCode = 404;
-           res.setHeader('Content-Type','application/json');
-          return res.end(JSON.stringify({msg:'User already exist! '}));
-        }
-      }
-
-      fs.writeFileSync(`./userfiles/${jsonObject.phone}.txt`,fullBodyAsString);
-
-      res.statusCode=200;
-      res.setHeader('Content-Type','application/json');
-      return res.end(JSON.stringify({msg: 'User Created!'}));
-        
-    });
-
-        
-
+    
 } else if(req.url === '/update-user' && req.method === 'PATCH'){
       
   } else if(req.url === '/read-user' && req.method === 'GET'){
---
+++ backend/userservice.js
@@ -0,0 +1,34 @@
+export default function createUser(req) {
+    const body = [];
+
+    req.on('data', (chunck)=>{
+        body.push(chunck);
+    });
+
+    req.on('end',async ()=>  {
+      const fullBodyAsString = Buffer.concat(body).toString(); 
+
+      const jsonObject = JSON.parse(fullBodyAsString);
+
+      const count = fs.readdirSync('./userfiles',(err,data)=>{
+        return data;
+      });
+
+
+      for(let file of count){
+        if(file==`${jsonObject.phone}.txt`){
+           res.statusCode = 404;
+           res.setHeader('Content-Type','application/json');
+          return res.end(JSON.stringify({msg:'User already exist! '}));
+        }
+      }
+
+      fs.writeFileSync(`./userfiles/${jsonObject.phone}.txt`,fullBodyAsString);
+
+      res.statusCode=200;
+      res.setHeader('Content-Type','application/json');
+      return res.end(JSON.stringify({msg: 'User Created!'}));
+        
+    });
+
+}
